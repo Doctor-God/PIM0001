8b5189d9798a6685a556163b4f0310935f9cdb8d
Doctor-God <mycrazysideyt@hotmail.com>
2018-11-19 15:09:47 -0200

    Quase funciona com moedas reais

--- Morfologia/contaMoedas.py
+++ Morfologia/contaMoedas.py
@@ -5,6 +5,7 @@ import cv2
 import sys
 import morfo as mo
 import segmenta as seg
+import matplotlib.pyplot as plt
 
 def thresholding(img, limite):
     out = np.zeros(img.shape, np.uint8)
@@ -12,58 +13,70 @@ def thresholding(img, limite):
         for y in range(img.shape[0]):
             if(img[y,x] < limite):
                 out[y,x] = 255
+
+    elem = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (50,50))
+
+    out = cv2.dilate(out, elem, iterations=1)
+    out = cv2.erode(out, elem, iterations=1)
+    out = cv2.dilate(out, elem, iterations=1)
+    out = cv2.erode(out, elem, iterations=1)
+
     return out
 
 def conta(img, limite, verbose=False):
-	elem_1r = cv2.imread("Imagens/moeda_1r_elem.jpg", 0)
-	elem_50 = cv2.imread("Imagens/moeda_50_elem.jpg", 0)
-	elem_25 = cv2.imread("Imagens/moeda_25_elem.jpg", 0)
-	elem_10 = cv2.imread("Imagens/moeda_10_elem.jpg", 0)
-	elem_5 = cv2.imread("Imagens/moeda_5_elem.jpg", 0)
+    elem_1r = cv2.imread("Imagens_reais/moeda_1r_elem.jpg", 0)
+    elem_50 = cv2.imread("Imagens_reais/moeda_50_elem.jpg", 0)
+    elem_25 = cv2.imread("Imagens_reais/moeda_25_elem.jpg", 0)
+    elem_10 = cv2.imread("Imagens_reais/moeda_10_elem.jpg", 0)
+    elem_5 = cv2.imread("Imagens_reais/moeda_5_elem.jpg", 0)
+    plt.hist(img.ravel(),256,[0,256]); plt.show()
 
-	img_lim = thresholding(img, limite)
-	if(verbose):
-		cv2.imshow("Imagem limiarizada", img_lim)
-		cv2.waitKey()
-		cv2.destroyAllWindows()
+    img_lim = thresholding(img, limite)
+
+    if(verbose):
+        sized = cv2.resize(img_lim, (960, 540))
+        cv2.imshow("Imagem limiarizada", sized)
+        cv2.waitKey()
+        cv2.destroyAllWindows()
 
 
-	temp = cv2.erode(img_lim, elem_25, iterations=1)
-	num_1r = seg.segmenta(temp)
-	temp = cv2.dilate(temp, elem_25, iterations=1)
-	print num_1r
-	if(verbose):
-		cv2.imshow("Moedas 1r", temp)
-		cv2.waitKey()
-		cv2.destroyAllWindows()
-
-	temp = cv2.erode(img_lim, elem_50, iterations=1)
-	num_25 = seg.segmenta(temp) - num_1r
-	temp = cv2.dilate(temp, elem_50, iterations=1)
-	print num_25
-	if(verbose):
-		cv2.imshow("Moedas 1r e 25", temp)
-		cv2.waitKey()
-		cv2.destroyAllWindows()
-
-	temp = cv2.erode(img_lim, elem_5, iterations=1)
-	num_50 = seg.segmenta(temp) - (num_1r+num_25)
-	temp = cv2.dilate(temp, elem_5, iterations=1)
-	print num_50
-
-	if(verbose):
-		cv2.imshow("Moedas 1r, 25 e 50", temp)
-		cv2.waitKey()
-		cv2.destroyAllWindows()
-
-	temp = cv2.erode(img_lim, elem_10, iterations=1)
-	num_5 = seg.segmenta(temp) - (num_1r + num_25 + num_50)
-	temp = cv2.dilate(temp, elem_10, iterations=1)
-	print num_5
-	if(verbose):
-		cv2.imshow("Moedas 1r, 25, 50 e 5", temp)
-		cv2.waitKey()
-		cv2.destroyAllWindows()
+    temp = cv2.erode(img_lim, elem_5, iterations=1)
+    num_1r = seg.segmenta(temp)
+    temp = cv2.dilate(temp, elem_5, iterations=1)
+    print num_1r
+    if(verbose):
+        sized = cv2.resize(temp, (960, 540))
+        cv2.imshow("Moedas 1r", sized)
+        cv2.waitKey()
+        cv2.destroyAllWindows()
+    #
+    #     temp = cv2.erode(img_lim, elem_50, iterations=1)
+    #     num_25 = seg.segmenta(temp) - num_1r
+    #     temp = cv2.dilate(temp, elem_50, iterations=1)
+    #     print num_25
+    #     if(verbose):
+    #         cv2.imshow("Moedas 1r e 25", temp)
+    #         cv2.waitKey()
+    #         cv2.destroyAllWindows()
+    #
+    #         temp = cv2.erode(img_lim, elem_5, iterations=1)
+    #         num_50 = seg.segmenta(temp) - (num_1r+num_25)
+    #         temp = cv2.dilate(temp, elem_5, iterations=1)
+    #         print num_50
+    #
+    #         if(verbose):
+    #             cv2.imshow("Moedas 1r, 25 e 50", temp)
+    #             cv2.waitKey()
+    #             cv2.destroyAllWindows()
+	# temp = cv2.erode(img_lim, elem_10, iterations=1)
+	# num_5 = seg.segmenta(temp) - (num_1r + num_25 + num_50)
+	# temp = cv2.dilate(temp, elem_10, iterations=1)
+	# print num_5
+    # if(verbose):
+    #     sized = cv2.resize(temp, (960, 540))
+    # 	cv2.imshow("Moedas 1r, 25, 50 e 5", sized)
+    # 	cv2.waitKey()
+    # 	cv2.destroyAllWindows()
 
 	num_10 = seg.segmenta(img_lim) - (num_1r + num_25 + num_50 + num_5)
 	print num_10
--- Morfologia/geraElementoMoeda.py
+++ Morfologia/geraElementoMoeda.py
@@ -5,83 +5,54 @@ import sys
 import morfo as mo
 import matplotlib.pyplot as plt
 
-def borderLim(img):
-    def borderize(img, a, b, out):
-        if(out[a, b] == 0):
-            return
-        pilha.append((a,b))
-        while(pilha != []):
-            (y,x) = pilha.pop()
-            out[y,x] = 0
-            if(y-1 >= 0):
-                if((img[y-1,x] >= img[y,x] -1 and img[y-1,x] <= img[y,x] +1) and out[y-1, x] != 0):
-                    pilha.append((y-1,x))
-            if(y+1 < img.shape[0]):
-                if((img[y+1,x] >= img[y,x] -1 and img[y+1,x] <= img[y,x] +1) and out[y+1, x] != 0):
-                    pilha.append((y+1,x))
-            if(x-1 >= 0):
-                if((img[y,x-1] >= img[y,x] -1 and img[y,x-1] <= img[y,x] +1) and out[y, x-1] != 0):
-                    pilha.append((y,x-1))
-            if(x+1 < img.shape[1]):
-                if((img[y,x+1] >= img[y,x] -1 and img[y,x+1] <= img[y,x] +1) and out[y, x+1] != 0):
-                    pilha.append((y,x+1))
+def geraElemento(img, limite):
 
-    out = np.ones(img.shape, np.uint8)
-    pilha = []
-    borderize(img, 0, 0, out)
-    print("oi")
-    borderize(img, 0, img.shape[1]-1, out)
-    print("oi")
-    borderize(img, img.shape[1]-1, 0, out)
-    print("oi")
-    borderize(img, img.shape[1]-1, img.shape[1]-1, out)
-
-    return out
-
-
-
-def geraElemento(img, dilat, limite):
-
-    # plt.hist(img.ravel(),256,[0,256]); plt.show()
+    plt.hist(img.ravel(),256,[0,256]); plt.show()
 
     temp = np.copy(img)
 
-    temp = borderLim(temp)
 
-    cv2.imshow("oi", temp)
-    cv2.waitKey()
-    cv2.destroyAllWindows()
+    # cv2.imshow("oi", temp)
+    # cv2.waitKey()
+    # cv2.destroyAllWindows()
 
-    # x_min = 0
-    # x_max = 0
-    # y_max = 0
-    # y_min = 0
+    x_min = 0
+    x_max = 0
+    y_max = 0
+    y_min = 0
 
-    # for x in range(img.shape[1]):
-    #     for y in range(img.shape[0]):
-    #         if(img[y,x] < limite):
-    #             temp[y,x] = 255
-    #             if(x < x_min):
-    #                 x_min = x
-    #             if(x > x_max):
-    #                 x_max = x
-    #             if(y < y_min):
-    #                 y_min = y
-    #             if(y > y_max):
-    #                 y_max = y
-    #         else:
-    #             temp[y,x] = 0
-    # out = mo.erode(temp, dilat)
+    for x in range(img.shape[1]):
+        for y in range(img.shape[0]):
+            if(img[y,x] < limite):
+                temp[y,x] = 255
+                if(x < x_min):
+                    x_min = x
+                if(x > x_max):
+                    x_max = x
+                if(y < y_min):
+                    y_min = y
+                if(y > y_max):
+                    y_max = y
+            else:
+                temp[y,x] = 0
+    elem = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (20,20))
+    elem2 = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (50,50))
+
+    temp = cv2.dilate(temp, elem, iterations=1)
+    temp = cv2.erode(temp, elem, iterations=1)
+    temp = cv2.dilate(temp, elem, iterations=1)
+    out = cv2.erode(temp, elem, iterations=1)
+    out = cv2.erode(out, elem2, iterations = 1)
     # out = temp
-    
+    #
     # y_par = False
     # x_par = False
     # if(out.shape[0] % 2 == 0):
     #     y_par = True
     # if(out.shape[1] % 2 == 0):
     #     x_par = True
-
-
+    #
+    #
     # if(y_par and x_par):
     #     new_out = np.zeros((out.shape[0]+1, out.shape[1]+1), np.uint8)
     # elif(y_par):
@@ -90,14 +61,14 @@ def geraElemento(img, dilat, limite):
     #     new_out = np.zeros((out.shape[0], out.shape[1]+1), np.uint8)
     # else:
     #     new_out = np.zeros((out.shape[0], out.shape[1]), np.uint8)
-
-
+    #
+    #
     # new_out[0:out.shape[0], 0:out.shape[1]] = out
-
+    #
     # cv2.imshow("oi", out)
     # cv2.waitKey()
     # cv2.destroyAllWindows()
-
+    #
     # out = np.copy(temp[y_min:y_max, x_min:x_max])
     # out = np.where(out < limite, 0, 255)
     # return new_out
@@ -110,7 +81,6 @@ if __name__ == "__main__":
     #argv[3] = threshold para limiarização
     argv = sys.argv
     img = cv2.imread(argv[1], 0)
-    dilatador = cv2.imread(argv[2], 0)
     name_ext = argv[1].split('.')
-    elem = geraElemento(img, dilatador, int(argv[3]))
+    elem = geraElemento(img, int(argv[2]))
     cv2.imwrite(name_ext[0] + "_elem." + name_ext[1], elem)
